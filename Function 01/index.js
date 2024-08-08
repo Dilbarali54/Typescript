fa2698f5324833f564c61dba1a7e2bac7045158c
Dilbar Ali <160045634+Dilbarali54@users.noreply.github.com>
2024-08-08 15:22:09 +0500

    Functions

---
+++ Function 01/index.js
@@ -0,0 +1,49 @@
+// ...... Funcation................
+// import { log } from "console";
+// function myFan(){
+//     console.log("Hello Dilbar how are are you kese ho ")
+//     console.log("Main te tehek a abba tu sudaa potter");
+// }
+// myFan()
+// function myId(){
+//     let my_first:string="Dilbar ali"
+//     let my_Second: string="Hahmi"
+//     console.log(my_first + my_Second);
+// }
+// myId()
+// function myNumber(){
+//     let x = 23;
+//     let y = 34;
+//     let sum = x + y;
+//     console.log(sum)
+// }
+// myNumber()
+// function Forcemass(){
+//     let a = 20;
+//     let m = 10;
+//     let force = 20*10;
+//     console.log(force);
+// }
+// Forcemass()
+// function mySum(a:number , b:number){
+//     let sum = a + b;
+//     console.log(sum);
+// }
+// .............. Class Work ..............
+// .......... Function Return .............
+// function myName(Egg:number,butter:number,Salt:number){
+//     return Egg + butter + Salt
+// }
+// let response:number=myName(1,1.5,2)
+// console.log(response);
+// function halfryEgg(Bread:number, Potato:number , Apple:number){
+//     return Bread + Potato + Apple
+// }
+// let response:number=halfryEgg(3,6,8)
+// console.log(response);
+function eggFry(Salt, Mirch, Oil) {
+    return Salt + Mirch + Oil;
+}
+let response = eggFry(1.5, 1.5, 1.5);
+console.log(response);
+export {};
---
+++ Function 01/index.ts
@@ -0,0 +1,82 @@
+// ...... Funcation................
+
+// import { log } from "console";
+
+// function myFan(){
+//     console.log("Hello Dilbar how are are you kese ho ")
+//     console.log("Main te tehek a abba tu sudaa potter");
+// }
+// myFan()
+
+// function myId(){
+//     let my_first:string="Dilbar ali"
+//     let my_Second: string="Hahmi"
+//     console.log(my_first + my_Second);
+// }
+// myId()
+
+// function myNumber(){
+//     let x = 23;
+//     let y = 34;
+//     let sum = x + y;
+//     console.log(sum)
+// }
+// myNumber()
+
+// function Forcemass(){
+//     let a = 20;
+//     let m = 10;
+//     let force = 20*10;
+//     console.log(force);
+// }
+// Forcemass()
+// function mySum(a:number , b:number){
+//     let sum = a + b;
+//     console.log(sum);
+    
+// }
+
+// .............. Class Work ..............
+  
+// .......... Function Return .............
+
+// function myName(Egg:number,butter:number,Salt:number){
+//     return Egg + butter + Salt
+// }
+// let response:number=myName(1,1.5,2)
+// console.log(response);
+
+// function halfryEgg(Bread:number, Potato:number , Apple:number){
+//     return Bread + Potato + Apple
+// }
+// let response:number=halfryEgg(3,6,8)
+// console.log(response);
+
+// function eggFry(Salt:number , Mirch:number , Oil:number){
+//     return Salt + Mirch + Oil
+// }
+// let response:number=eggFry(1.5,1.5,1.5)
+// console.log(response);
+
+// .............. Hosting Function ...........
+// Global Scope
+// var name1="dilbar";
+// let name2="azhar";
+// const name3="ali";
+
+// ...... Block Scope ........
+
+// function functionType(num1:number , num2:number){
+//       return num1 + num2
+// }
+// let userName:string ="Kamrn khan tessori";
+// let userDesignation:string = " Governor Sindh ";
+// let userResidence: string = "Governor House";
+
+// let userObject={
+//     name:"Kamran khan tessori",
+//     Designation:"Governor Sindh ",
+//     Residence:"Governor Sindh",
+// }
+// console.log(`});
+
