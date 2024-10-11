b349bdbf4ef4a092ba193d07cb2f65a861690282
Dilbar Ali <160045634+Dilbarali54@users.noreply.github.com>
2024-10-11 17:54:34 +0500

    Add files via upload

---
+++ Practice Session-01/index.js
@@ -0,0 +1,230 @@
+//......... String data types..........
+// import { Console } from "console";
+// console.log("Hello Dilbar ali");
+// console.log("Kaha ho mr nazar he nahi a rahe hoo");
+// console.log("idher he apke badsahi me ");
+// console.log("nazar tu nahi atte hoo!");
+// console.log("koi bachee sett kee hai kiya");
+// console.log("tu uske friend se baat kre gaa chall uskaa number dee ok ");
+// console.log("0311-2235475");
+// console.log("use nahi batana mauiene number diyaa hai 0319749829209");
+// ...........Number Data types.............
+// console.log(1289);
+// console.log(890);
+// console.log(1000);
+// console.log(4266);
+// console.log(12568);
+// ............ Check of data typfio..........
+// console.log(typeof "Dilbar hashmi");
+// console.log(typeof 123);
+// console.log(typeof true);
+// console.log(typeof ["mth","physic","biology"]);
+// ............. Boolean Data types...........
+// console.log(true);
+// console.log(false);
+// console.log(2 === 2);
+// ........... Concatenation and Literals ...........
+// let myName:string="Dilbar hashmi"
+// let age:number = 22;
+// console.log(`My name is ${myName} i am ${age} years old`);
+// let myId:string="Ali khan"
+// let old:number = 15;
+// console.log(`My name is ${myId} i am ${old} years old`);
+// console.log(`My name is dilbar`+`i am student of governor sindh it`+`from kandh kot and sindh side`);
+// .............. Variables Let Const Var ................
+// let myId:string= "apple"
+// mySay:" Orange"
+// let myPassion: string="My self dilbar ali insahallah one day to be programmer "
+// console.log(myId);
+// const Id_number: string ="my first and last name is dilbar that is it clear!"
+// console.log(Id_number);
+// ................. Variable Naming Convention ..............
+// let mySide:string ="the city off sukkur"  Camel Case
+// let box_side_and_name:number = "flowers"   Sanke Case
+// let MyBoxOneInTheRoom:string ="Rose"    Pascal Case
+// ................ Updation ...................
+// let myId:string ="ali"  declareation
+// myBox:"apple"      update
+// mySon:"Affan ali"
+// ................. Block Scope / Global Scope ........
+// let x = 10;
+// if(x === 10){
+//     let myBox:string="flowers"
+//     const myName:string="Dilbar"
+//     var myId:string="Tum hoo "
+//     console.log(myBox);
+// }
+// .................. The End of Practice Session 01 ............................
+// .......................  Start of Practice Seaaion  02 ........................
+//  .......... Opertors ............
+//  Addition
+// console.log(3+4);
+// console.log(9+8);
+// console.log(87+34);
+// // Substracion
+// let num1:number=23;
+// let num2:number=8;
+// let manas=23-8;
+// console.log(manas);
+//  Multiplication
+// let num1:number=8;
+// let num2:number=9;
+// let sum=8 * 9;
+// console.log(sum);
+// let num3:number=78;
+// let num4:number=15;
+// let Multiplication= 78 *15
+// console.log(Multiplication);
+// .......... Relation / Comprasion Operators ..................
+// let num1=20;
+// let num2=10;
+// let num3='5';
+// let num4=8;
+// let num5=5;
+// console.log(num1 == num2); it is false but 10 is not equal to 20
+// console.log(num2 == num4);
+// console.log(num3 === num5);
+// less than
+// console.log(num1 < num2);
+// ======= Increment and Decrement ================
+// Q 23
+// let myFruit:string="Grapes";
+// console.log(`Is myFruit = "Grapes"? I predict is true`);
+// console.log(myFruit === "Grapes");
+//  test 2
+// let age = 25;
+// console.log(`Is age = 25 I predict is false`);
+// console.log(age<20);
+// test 3
+// let age = 40;
+// console.log(`Is age = 40 I predict is false`);
+// console.log(age<14);
+// test 4
+// console.log(`Is age = 40 I predict is Wrong`);
+// console.log(age<35);
+// // test 5
+// console.log(`Is age = 40 I predict is Correct`);
+// console.log(age>10);
+// console.log(`Is age 40 ? I predict is Right`);
+// console.log(age>39);
+// console.log(age !== 35);
+// console.log(age !==45);
+// ======= Topic Increment and decrement ===========
+// increment
+//  let x=200;
+// //   x= x +1;
+//   ++x;
+//    console.log(x);
+// pre increment phale 
+// let x = 200;
+// ++x;
+// console.log(++x); //2001
+// console.log(++x); // 2002
+// let num1=++x;
+// console.log(x);
+// let y = 300;
+// y++;
+// console.log(y);
+// post increment baad main
+// x++;
+// console.log(x++);//200
+// console.log(x++);   // 201
+// console.log(x)
+// let num4 = x++;
+//  num4 = x;
+// console.log(num4);
+// Decrement
+// let y = 400;
+//    y--;
+//    console.log(y);
+// --y;
+// console.log(y);
+// practice 
+// let number1 = 30;
+// console.log((++number1) + (++number1));
+//         //   31   +         31
+//    let number2= 40;
+//    console.log((++number2) + (++number2));
+//    let number3= 50;
+//    console.log((++number3) + (number3));
+//  console.log((number3++) + (number3++) + (number3++));
+// console.log((number3++) + (number3++));
+//  ==========  The End  Practice Seasion 02 ============
+//  ============= Practice Seassion 03 ================
+// ............... Topic ; And && ..........................
+// Ans ; ager dono side true statements hogee and doble && hoga tu (true) ayee ga
+//       ager dono main se ek pe true or dosre side false tu run main (false) ayee gaa 
+// let x=15;
+// let y=15;
+// console.log(true && true);
+// console.log(false && true);
+// console.log(false && false);
+// console.log(true && false);
+// console.log(x<20 && y>10);
+// let y = 20;
+// let z =25;
+// console.log(y<25 && z>15);
+// let a =30;
+// let b =40;
+// console.log(a<25 && b>30);
+// let c =60;
+// let d =80;
+// console.log(c<65 && d>75);
+// let e = 90;
+// let f=55;
+// console.log(e<95 && f>50);
+//  OR || Iska matlab hai ke dono side false hoga phir false de ga ager ek side true hoga dosre side false ho phir bhe ye true de gaa
+// let a = 30;
+// let b= 10;
+// console.log(true || true);
+// console.log(false || false);
+// console.log(false || true );
+// console.log(a === 30 || b === 8);
+// console.log(a === 28 || b === 7);
+// console.log(a === 30 || b === 10);
+// console.log(!true === true);
+// console.log(!true === false);
+// ======== Condiational Satetment ==============
+// let personAge = 20 ;
+// if(personAge>18){
+//     console.log("He can Vote");
+// }
+// let myBox=70;
+// if(myBox>65){
+//     console.log("please show me correct answer");
+// }
+// let temperature=30;
+// if(temperature>40){
+//     console.log("the weather is hot");
+// }else{
+//     console.log("the weather is cold");
+// }
+// let today=40;
+// if(today>49){
+//     console.log("today weather is cold");
+// }else{
+//    console.log("tommorow is weather very hot"); 
+// }
+// let myId=25;
+// if(myId>28){
+//     console.log("I am not satsfies");
+// }else if(myId>20){
+//     console.log("Today my feeling not well");
+// }else{
+//     console.log("I am so happy today");
+// }
+let ram = "8gb";
+let generation = "9th";
+let storage = "248gb";
+// if(ram=== "7gb" && storage === "258gb"){
+//     console.log("I buy this laptop");
+// }else{
+//     console.log("I can not buy this laptop");
+// }
+if (ram === "7gb" || storage === "238") {
+    console.log("I buy this i phone");
+}
+else {
+    console.log("I can not buy this i phone");
+}
+export {};
